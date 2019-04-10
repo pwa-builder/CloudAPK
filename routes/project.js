612316cd15197ec5f36f869bcfdbd02dde58265a
marianstdt <mariano.stinson@southworks.com>
2019-04-10 17:43:15 -0300

    Added placeholder for filesize limit

--- routes/project.js
+++ routes/project.js
@@ -6,7 +6,10 @@
  const storage = multer.diskStorage({
      destination: function (req, file, cb) {
          cb(null, './uploads/');
-     },
+     }, 
+    //  limits: {
+    //     fileSize: 1024 * 1024 * 5
+    //   },
      filename: function (req, file, cb) {
          cb(null, file.originalname);
      }
