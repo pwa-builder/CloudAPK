a12599ba01448cf144d5c9beecce93483827c665
marianstdt <mariano.stinson@southworks.com>
2019-04-12 16:56:35 -0300

    changed route on buildAPK

--- build/buildAPK.js
+++ build/buildAPK.js
@@ -3,8 +3,10 @@ const path = require('path');
 
 //This functions generates an unsigned Android apk. 
 module.exports = (projectFolder) => {
-    const buildPath = `/app/${projectFolder}/projects/Polyfills/android/source`;
+    const buildPath = `${projectFolder}/projects/Polyfills/android/source`;
     const command = `cd ${buildPath} && /opt/gradle/gradle-5.3.1/bin/gradle assemblerelease`;
     await runCommand(command);
+
     return path.join(buildPath, '/app/build/outputs/apk/release/app-release-unsigned.apk');
+
 };
