af0e80a2434dd78fdce7f3ffd18be2438c8fa830
marianstdt <mariano.stinson@southworks.com>
2019-04-15 10:25:31 -0300

    fix require and async in built apk

--- build/buildAPK.js
+++ build/buildAPK.js
@@ -1,8 +1,8 @@
-const runCommand = require('./runCommand').default.default;
+const runCommand = require('./runCommand');
 const path = require('path');
 
 //This functions generates an unsigned Android apk. 
-module.exports = (projectFolder) => {
+module.exports = async (projectFolder) => {
     const buildPath = `${projectFolder}/projects/Polyfills/android/source`;
     const command = `cd ${buildPath} && /opt/gradle/gradle-5.3.1/bin/gradle assemblerelease`;
     await runCommand(command);
