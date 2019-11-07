a50eaa06ca2416c280f67bdb2392b9353919d1ca
Justin Willis <jgw9617@gmail.com>
2019-11-07 13:45:27 -0800

    fix(): change buildPath to valid path

--- build/buildAPK.js
+++ build/buildAPK.js
@@ -2,7 +2,7 @@ const runCommand = require('./runCommand');
 const path = require('path');
 
 module.exports = async (projectFolder) => {
-    const buildPath = `${projectFolder}/projects/Polyfills/android/source`;
+    const buildPath = `${projectFolder}/projects/android-twa/source`;
     const command = `cd ${buildPath} && /opt/gradle/gradle-5.3.1/bin/gradle assemblerelease`;
     
     await runCommand(command);
