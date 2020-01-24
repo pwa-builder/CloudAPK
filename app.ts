75be3150928971eb0ecbd085af447fe95e213bea
Justin Willis <jgw9617@gmail.com>
2020-01-23 22:03:19 -0800

    fix(): support cors

--- app.ts
+++ app.ts
@@ -2,8 +2,10 @@ import express from 'express';
 const app = express();
 const routes = require('./routes/project');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/', routes);
 
 module.exports = app;
