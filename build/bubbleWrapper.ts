18fcafa9f89fb912b39a086c5bf99fbcb0d6c264
Judah Gabriel Himango <judahgabriel@gmail.com>
2020-04-06 15:57:40 -0700

    Removed twa manfiefst serialization

--- build/bubbleWrapper.ts
+++ build/bubbleWrapper.ts
@@ -55,7 +55,7 @@ export class BubbleWrapper {
         const manifestSettings = Object.assign({}, this.pwaSettings)
         const twaManifest = new TwaManifest(this.createManifestSettings(this.pwaSettings, this.signingKeyInfo));
         twaManifest.generatorApp = "PWABuilder";
-        await twaManifest.saveToFile(this.projectDirectory + "/twa-manifest.json");
+        //await twaManifest.saveToFile(this.projectDirectory + "/twa-manifest.json");
         await twaGenerator.createTwaProject(this.projectDirectory, twaManifest);
         return twaManifest;
     }
