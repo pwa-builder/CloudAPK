5f94e625f3e2167609f3731082f786cc72a77931
Judah Gabriel Himango <judahgabriel@gmail.com>
2020-06-04 21:02:45 -0700

    Removed temp deploy test code

--- routes/project.ts
+++ routes/project.ts
@@ -7,26 +7,12 @@ import tmp, { dir } from "tmp";
 import archiver from "archiver";
 import fs from "fs-extra";
 import { SigningKeyInfo } from "../build/signingKeyInfo";
-import fetch from "node-fetch";
 import del from "del";
 
 const router = express.Router();
 
 const tempFileRemovalTimeoutMs = 1000 * 60 * 30; // 30 minutes
 tmp.setGracefulCleanup(); // remove any tmp file artifacts on process exit
-
-router.get("/fetchTest", async function (request: express.Request, response: express.Response) {
-  console.log("fetching...");
-  try {
-    var result = await fetch("https://sadchonks.com/kitteh-512.png");
-    var buffer = await result.buffer();
-    console.log("successfully got buffer", buffer.length);
-    response.send("success: " + buffer.length.toString());
-  } catch (fetchErr) {
-    console.log("fetch err: ", fetchErr);
-    response.send("fetch err: " + fetchErr);
-  }
-});
 
 /**
  * Generates and sends back a signed .apk. Expects a POST body containing @see PwaSettings object.
