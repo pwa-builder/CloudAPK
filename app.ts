9b8ef502ff319806b9851ada38e856f0de6d408d
Amrutha Srinivasan <amsrin@microsoft.com>
2022-06-16 11:53:11 -0700

    Cors reverted

--- app.ts
+++ app.ts
@@ -5,11 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: '*',
-  })
-);
+app.use(cors());
 app.use('/', routes);
 app.use(express.static('static'));
 
